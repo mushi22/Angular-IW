0d16da8accf62bcf2076dd78e7606e41cd3bef95
mushi22 <mushi22@github.com>
2014-12-31 20:55:16 +1100

    login still not working argh!

--- app/js/app.js
+++ app/js/app.js
@@ -7,10 +7,18 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'firebase'
 ]).
 config(['$routeProvider', function($routeProvider) {
   //$routeProvider.when('', {templateUrl: 'index.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/', {templateUrl: 'index.html', controller: 'indexController'});
+  $routeProvider.when('/', {
+  	templateUrl: 'index.html', 
+  	controller: 'indexController'
+  });
+  $routeProvider.when('login', {
+  	templateUrl: 'login.html',
+  	controller: 'AuthController'
+  });
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,4 +5,32 @@
 angular.module('myApp.controllers', [])
   .controller('indexController', [function() {
 
+  }])
+
+
+  .controller('AuthController', ['$scope',  function($scope) {
+
+  	Parse.initialize("vBbvihMsKuxIfMc999TtrwCHAKxRW2PMarjtRVBn","Y53eTAGBxDeiWNDFOwomUhDFjlEnh2qEiAiY9GOi");
+  	console.log('test');
+
+  	$scope.user = {username: '', password: ''};
+  	$scope.login = function() {
+  		console.log("test");
+  		Parse.User.logIn($scope.user, $scope.passwd, {
+  			success: function(user) {
+    			// Do stuff after successful login.
+    			console.log(user);
+    			console.log("successful login");
+    			$location.path('/');
+  			},
+  			error: function(user, error) {
+    			// The login failed. Check error to see why.
+    			consle.log(user);
+    			console.log(error);
+    			console.log("login Failed");
+  			}
+		});
+
+  	};
+
   }]);
